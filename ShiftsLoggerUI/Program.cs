a8434d5aaddb7e82a597588cd318e4871654daad
Gustav Polhem <gustavpolhem@gmail.com>
2024-05-14 12:41:33 +0200

    fix

--- ShiftsLoggerUI/Program.cs
+++ ShiftsLoggerUI/Program.cs
@@ -5,8 +5,6 @@ namespace ShiftsLoggerUI;
 
 public class Program
 {
-    private readonly ShiftContext _context;
-
     public static async Task Main(string[] args)
     {
         var userInput = new UserInput();
